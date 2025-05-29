b5562a54e0d3511341518017d0b26b5cdf9d80bf
larckot <marianosd10@gmail.com>
2025-05-29 19:57:00 -0300

    fix

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import transcriptionRoutes from './app/routes/transcription.js';
+import authRoutes from './app/routes/auth.js';
 
 const app = express()
 
 dotenv.config();
 
 const port = 3000
+
+app.use('/auth', authRoutes);
 
 app.use('/transcription', transcriptionRoutes);
 
